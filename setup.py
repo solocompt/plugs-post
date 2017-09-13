54a754d2653435c6ee33de22d77581e922c2c19e
Ricardo Lobo <ricardolobo@audienciazero.org>
2017-09-13 22:35:53 +0100

    bumps version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "plugs_core>=0.1.6,<0.3.0"
+        "plugs_core>=0.1.6,<1.0.0"
     ],
     license="MIT",
     zip_safe=False,
